249b6d20265993b04822c1c6f554de2eb0ce74d7
a-hodges <16946799+a-hodges@users.noreply.github.com>
2024-09-24 13:07:22 -0700

    Auto zip

--- build.js
+++ build.js
@@ -1,11 +1,19 @@
 import { compilePack } from "@foundryvtt/foundryvtt-cli";
+import AdmZip from "adm-zip";
 
 const packs = ["actors", "items", "macros", "tables"];
 
 const promises = [];
 for (const dir of packs) {
-  const src = `packs/src/${dir}`;
+  const src = `src/${dir}`;
   const out = `packs/${dir}`;
   promises.push(compilePack(src, out));
 }
 await Promise.all(promises);
+
+const zip = new AdmZip();
+for (const file of ["LICENSE", "module.json", "README.md"]) {
+  zip.addLocalFile(file);
+}
+zip.addLocalFolder("./packs", "/packs");
+zip.writeZip("./packs.zip");
