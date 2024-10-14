c8441a7a73f5c02beb4aa94ef0f6d256bbfee942
a-hodges <16946799+a-hodges@users.noreply.github.com>
2024-10-14 11:51:35 -0700

    Update build.js to remove packs folder

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
+import fs from "fs";
 import { compilePack } from "@foundryvtt/foundryvtt-cli";
 import AdmZip from "adm-zip";
 
+const pack_path = "./packs";
 const packs = ["actions", "actors", "effects", "feats", "items", "macros", "tables"];
 
 const promises = [];
 for (const dir of packs) {
   const src = `src/${dir}`;
-  const out = `packs/${dir}`;
+  const out = `${pack_path}/${dir}`;
   promises.push(compilePack(src, out, { log: true, recursive: true }));
 }
 await Promise.all(promises);
@@ -15,5 +17,5 @@ const zip = new AdmZip();
 for (const file of ["LICENSE", "module.json", "README.md"]) {
   zip.addLocalFile(file);
 }
-zip.addLocalFolder("./packs", "/packs");
-zip.writeZip("./packs.zip");
+zip.addLocalFolder(pack_path, "/packs");
+zip.writeZip("./packs.zip", (_) => fs.rmSync(pack_path, { recursive: true, force: true }));
